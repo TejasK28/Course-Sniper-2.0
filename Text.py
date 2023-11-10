83f3027eb0dbdd6a98cab9e720b9c62a8b8208a9
Tejas <87278292+TejasK28@users.noreply.github.com>
2023-11-10 08:55:49 -0500

    Update Text.py

--- Text.py
+++ Text.py
@@ -2,14 +2,14 @@ import os
 from twilio.rest import Client
 
 def send(msg):   
-    account_sid = 'AC2c60b74c483f1166e6a24a7ad388368e' 
-    auth_token = '3edf3ebf68382e44f8edfec9e91b9d43'
+    account_sid = 'RED' 
+    auth_token = 'RED'
     client = Client(account_sid, auth_token)
  
     message = client.messages.create(
             body = msg,
-            from_='+18449594235',
-            to='+18482563099'
+            from_='+RED',
+            to='+RED'
         )
     
 send('hihi')
